9407b66731223962ec4d6b9b35804abd66200055
John Hunt <jeh@midmarsh.co.uk>
2020-03-26 17:40:51 +0000

    Lab illustraitng inheritance

---
+++ lab5-inheritance/bookshop.js
@@ -0,0 +1,81 @@
+class Product {
+  constructor(price) {
+  this.price = price;
+  this.discount = 0.0;
+  }
+
+  setSaleDiscount(discount) {
+    this.discount = discount;
+  }
+  calculateSalePrice() {
+    let discountAmount = this.price * this.discount;
+    return this.price - discountAmount;
+  }
+}
+
+class Book extends Product {
+  constructor(isbn, title, author, genre, publisher, price) {
+    super(price);
+    this.isbn = isbn;
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.publisher = publisher;
+  }
+  // Override default toString method
+  toString() {
+    return (
+      "Book(" +
+      this.isbn +
+      "," +
+      this.title +
+      ", " + 
+      this.author +
+      "," +
+      this.genre +
+      "," +
+      this.publisher +
+      "," +
+      this.price +
+      "," +
+      this.discount +
+      "," +
+      ")"
+    );
+  }
+}
+
+class Author {
+  constructor(firstname, surname) {
+    this.firstname = firstname;
+    this.surname = surname;
+  }
+  // Override default toString method
+  toString() {
+    return "Author(" + this.firstname + "," + this.surname + ")";
+  }
+}
+
+class Publisher {
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+  }
+  // Override default toString method
+  toString() {
+    return "Publisher(" + this.name + "," + this.address + ")";
+  }
+}
+
+console.log("Welcome to the Bookshop");
+
+let author = new Author("Pete", "Smith");
+let publisher = new Publisher("Tech Books Publishing Ltd.", "10 High Street");
+let book = new Book(1, "Go Unleashed", author, "Technical", publisher, 15.99);
+
+console.log(book);
+console.log(`${book}`);
+
+console.log("Calculating the Sales Discount price");
+book.setSaleDiscount(0.1);
+console.log("Sale price of book:", book.calculateSalePrice());
